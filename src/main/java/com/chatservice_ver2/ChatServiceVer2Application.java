2dab8762a475323728f26f093d9b453f154a56a5
오동현 <dhgus44@naver.com>
2023-10-06 20:53:33 +0900

    feat: chat

---
+++ src/main/java/com/chatservice_ver2/ChatServiceVer2Application.java
@@ -0,0 +1,13 @@
+package com.chatservice_ver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServiceVer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServiceVer2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/chatservice_ver2/repository/ChatRepository.java
@@ -0,0 +1,11 @@
+package com.chatservice_ver2.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Getter @Setter
+public class ChatRepository {
+
+}
